569125a126ce6ed044628af0e85f749fd2e48f60
s4911672 <36960868+s4911672@users.noreply.github.com>
2020-02-25 14:56:17 +0000

    not going to use the interaction engine, atm you can pick up the ship and it gets stuck to your hand

---
+++ Assets/Artefact_Hand_PickUp.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Artefact_Hand_PickUp : MonoBehaviour
+{
+
+    public Collider[] ObjectsInRadius;
+    public float radius;
+    public GameObject Palm;
+    public GameObject ArtefactObject;
+    public Vector3 ArtefactObject_StartLocation;
+    public Vector3 ArtefactObject_StartOrientation;
+    public GameObject ArtefactObject_Home;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GrippingObject()
+    {
+        ObjectsInRadius = Physics.OverlapSphere(Palm.gameObject.transform.position, radius);
+        for (int i = 0; i != ObjectsInRadius.Length; i++)
+        {
+            if (ObjectsInRadius[i].gameObject.tag == "Artefact")
+            {
+                ArtefactObject = ObjectsInRadius[i].gameObject;
+                
+            }
+
+        }
+
+        if (ArtefactObject != null)
+        {
+            ArtefactObject_StartLocation = ArtefactObject.transform.position;
+            ArtefactObject_StartOrientation = ArtefactObject.transform.localEulerAngles;
+            ArtefactObject_Home = ArtefactObject.gameObject.transform.parent.gameObject;
+            ArtefactObject.transform.parent = Palm.transform;
+            
+
+        }
+
+    }
+
+    public void UngrippingObject()
+    {
+
+        
+
+
+        
+        
+
+
+    }
+
+
+}

961fedff246fecb40e099fa6f3873e6a455b4f96
s4911672 <36960868+s4911672@users.noreply.github.com>
2020-02-27 14:59:09 +0000

    added some more info to Charles I Room, added picking up objects (mostly reliable)

--- Assets/Artefact_Hand_PickUp.cs
+++ Assets/Artefact_Hand_PickUp.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Artefact_Hand_PickUp : MonoBehaviour
 {
+
+
+    public GameObject DebugCube;
+    public GameObject ObjectToPickUp;
+    public Vector3 ArtefactObject_StartLocation;
+    public Vector3 ArtefactObject_StartOrientation;
+    public GameObject ArtefactObject_Home;
+    public GameObject Palm;
+
+    public GameObject ObjectHolding_Text;
+    /*
 
     public Collider[] ObjectsInRadius;
     public float radius;
     public GameObject Palm;
     public GameObject ArtefactObject;
-    public Vector3 ArtefactObject_StartLocation;
-    public Vector3 ArtefactObject_StartOrientation;
-    public GameObject ArtefactObject_Home;
-
+    
+    */
 
 
     // Start is called before the first frame update
@@ -26,9 +37,22 @@ public class Artefact_Hand_PickUp : MonoBehaviour
     {
         
     }
-
+    
     public void GrippingObject()
     {
+        DebugCube.GetComponent<Renderer>().material.color = Color.green; //visual debug
+
+        if (ObjectToPickUp != null)
+        {
+            ArtefactObject_StartLocation = ObjectToPickUp.transform.position;
+            ArtefactObject_StartOrientation = ObjectToPickUp.transform.localEulerAngles;
+            ArtefactObject_Home = ObjectToPickUp.gameObject.transform.parent.gameObject;
+            ObjectToPickUp.transform.parent = Palm.transform;
+            ObjectHolding_Text.GetComponent<TextMeshProUGUI>().text = ObjectToPickUp.gameObject.name;
+
+
+        }
+        /*
         Debug.Log("Hand Open");
         ObjectsInRadius = Physics.OverlapSphere(Palm.gameObject.transform.position, radius);
         for (int i = 0; i != ObjectsInRadius.Length; i++)
@@ -50,16 +74,31 @@ public class Artefact_Hand_PickUp : MonoBehaviour
             
 
         }
-
+        */
     }
 
     public void UngrippingObject()
     {
+        DebugCube.GetComponent<Renderer>().material.color = Color.blue; //visual debug
+        if (ObjectToPickUp != null)
+        {
+            
+            ObjectToPickUp.transform.parent = ArtefactObject_Home.transform;
+            ObjectToPickUp.transform.position = ArtefactObject_StartLocation;
+            ObjectToPickUp.transform.localEulerAngles = ArtefactObject_StartOrientation;
+            
+            //ObjectHolding_Text.GetComponent<Text>().text = null;
+            ObjectHolding_Text.GetComponent<TextMeshProUGUI>().text = null;
+            
+
+
+        }
         
+        /*
         ArtefactObject.transform.parent = null;
         ArtefactObject = null;
         ObjectsInRadius = new Collider[0];
-
+        */
 
 
 
@@ -67,6 +106,6 @@ public class Artefact_Hand_PickUp : MonoBehaviour
 
 
     }
-
+    
 
 }
---
+++ Assets/PickUpObject_Hand.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpObject_Hand : MonoBehaviour
+{
+    public GameObject DebugCube;
+    public GameObject PickUpController;
+
+    public Rigidbody RB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PickUpController = GameObject.FindGameObjectWithTag("PickUpController");
+        RB = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Right Hand" || other.gameObject.tag == "Left Hand") //if a hand is in the object's trigger area
+        {
+            DebugCube.GetComponent<Renderer>().material.color = Color.red; //visual debug
+            PickUpController.GetComponent<Artefact_Hand_PickUp>().ObjectToPickUp = gameObject;
+
+
+        }
+
+        
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Right Hand" || other.gameObject.tag == "Left Hand") //if a hand has left the objects trigger area
+        {
+            DebugCube.GetComponent<Renderer>().material.color = Color.white; //visual debug
+            PickUpController.GetComponent<Artefact_Hand_PickUp>().ObjectToPickUp = null;
+            
+        }
+    }
+}

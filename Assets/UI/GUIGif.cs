a4b95df3f6fa9da0dce862fee225059d0db0e8c0
Ethan_Desktop <36960868+s4911672@users.noreply.github.com>
2020-03-07 00:22:32 +0000

    added some gesture image things tp tell how to interact with it for the diorama objects. did some other things but not sure what

---
+++ Assets/UI/GUIGif.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIGif : MonoBehaviour
+{
+    public Sprite[] Frames;
+    public float WaitTime = 1;
+    public float CurrentTime;
+    public int StartFrame = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentTime = WaitTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        CurrentTime -= Time.deltaTime;
+
+        if (CurrentTime < 0)
+        {
+            StartFrame += 1;
+            if (StartFrame >= Frames.Length)
+            {
+                StartFrame = 0;
+
+            }
+            gameObject.GetComponent<Image>().sprite = (Frames[StartFrame]);
+            CurrentTime = WaitTime;
+        }
+        
+    }
+}

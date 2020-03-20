395fc255315138bda9eb5fd43477c3dd0556f76f
Ethan_Desktop <36960868+s4911672@users.noreply.github.com>
2020-03-20 17:19:51 +0000

    commit before dicking around with the steam vr teleport code

--- Assets/PlayerSpawning.cs
+++ Assets/PlayerSpawning.cs
@@ -6,89 +6,21 @@ using UnityEngine.SceneManagement;
 
 public class PlayerSpawning : MonoBehaviour
 {
+    public static bool FirstTimeSpawn = true;
     private void Start()
     {
-        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
-        if (SavedPositionManager.savedPositions.ContainsKey(sceneIndex))
+        if (FirstTimeSpawn == true)
         {
-            transform.position = SavedPositionManager.savedPositions[sceneIndex];
-        }
-    }
+            FirstTimeSpawn = false;
 
-    private void OnDestroy()
-    {
-        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SavedPositionManager.savedPositions[sceneIndex] = transform.position;
-    }
-    /*
-    //public static GameObject PlayerReturnPoint;
-    public static bool FirstSpawnInstance = true;
-    public GameObject FirstSpawnPoint;
-    public GameObject Player;
-
-
-    private void Awake()
-    {
-        Player = GameObject.FindGameObjectWithTag("Player");
-        if (FirstSpawnInstance == true)
-        {
-            FirstSpawnInstance = false;
-            
         }
 
-        else if (FirstSpawnInstance == false)
+        else if (FirstTimeSpawn == false)
         {
-            FirstSpawnPoint.SetActive(false);
-            Player.transform.position =(LoadLevel.PlayerReturnPoint);
-        }
-    }
-    public void NewSpawnPoint()
-    {
-        
-
-    }
-
-    
-    public GameObject PlayerSpawnPoint;
-    static public Vector3 SavedPosition;
-    static public bool FirstSpawnInstance = true;
-    public GameObject Player;
-  
-    // Start is called before the first frame update
-
-    
-    void Awake()
-    {
-        Player = GameObject.FindGameObjectWithTag("Player");
-        if (FirstSpawnInstance == true)
-        {
-
-            FirstSpawnInstance = false;
-
-        }
-        else if (FirstSpawnInstance == false)
-        {
-            PlayerSpawnPoint.transform.position = SavedPosition;
-
+            gameObject.SetActive(false);
         }
         
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-       
-       
-    }
-
-    public void SavePosition()
-    {
-        PlayerSpawnPoint.transform.position = Player.transform.position;
-        SavedPosition = PlayerSpawnPoint.transform.position;
-        
-    }
-
-    */
 
 
 }
---
+++ Assets/TeleportPointGesture.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class TeleportPointGesture : MonoBehaviour
+{
+    public bool IsPointing;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsPointing == true)
+        {
+            gameObject.GetComponent<TeleportArc>().Show();
+
+        }
+    }
+
+    public void GestureActive()
+    {
+        IsPointing = true;
+        Debug.Log("Teleport Gesture Active");
+    }
+    public void GestureInactive()
+    {
+        IsPointing = false;
+        Debug.Log("Teleport Gesture Inactive");
+    }
+}

bd7bfb4f4fc3196bb55aae33ba2f63b02cd667cb
Ethan_Desktop <36960868+s4911672@users.noreply.github.com>
2020-05-26 22:49:53 +0100

    fixing GUI scaling

---
+++ Assets/doorTest/DoorLevelLoad.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorLevelLoad : MonoBehaviour
+{
+    public bool OpenDoor;
+    public GameObject door;
+    public GameObject Room;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Room.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OpenDoor == true && Room.active == false)
+        {
+            gameObject.transform.Rotate(new Vector3 (0f, 90f, 0f));
+
+            Room.SetActive(true);
+
+            
+
+        }
+
+        
+    }
+}

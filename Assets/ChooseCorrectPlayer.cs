5f0012d62d20a0282d8839dab2efce0383c97f08
Ethan_Desktop <36960868+s4911672@users.noreply.github.com>
2020-05-27 00:20:07 +0100

    I think iv got a system so that the modalities dont need to be turned on and off manually anymore, but need to sort the build index out so diorama's work properly

--- Assets/ChooseCorrectPlayer.cs
+++ Assets/ChooseCorrectPlayer.cs
@@ -8,36 +8,43 @@ public class ChooseCorrectPlayer : MonoBehaviour
     public GameObject VRSRPlayer;
     public GameObject VRLMSRPlayer;
 
+    public GameObject Player;
+
+    public bool DebugSpawnS;
+    public bool DebugSpawnVR;
+    public bool DebugSpawnVRR;
+
     public GameObject ModalityController;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         ModalityController = GameObject.FindGameObjectWithTag("ModalityController");
         if (ModalityController.GetComponent<ModalityController2>().Gamepad_Chosen == true)
         {
+            Player =Instantiate(GamepadPlayer, gameObject.transform.position, gameObject.transform.rotation, transform.parent);
+            /*
             GamepadPlayer.SetActive(true);
             VRSRPlayer.SetActive(false);
             VRLMSRPlayer.SetActive(false);
+            */
         }
 
         else if (ModalityController.GetComponent<ModalityController2>().VRSR_Chosen == true)
         {
-            GamepadPlayer.SetActive(false);
-            VRSRPlayer.SetActive(true);
-            VRLMSRPlayer.SetActive(false);
+            Player = Instantiate(VRSRPlayer, gameObject.transform.position, gameObject.transform.rotation, transform.parent);
         }
 
         else if (ModalityController.GetComponent<ModalityController2>().VRLMSR_Chosen == true)
         {
-            GamepadPlayer.SetActive(false);
-            VRSRPlayer.SetActive(false);
-            VRLMSRPlayer.SetActive(true);
+            Player = Instantiate(VRLMSRPlayer, gameObject.transform.position, gameObject.transform.rotation, transform.parent);
         }
+
+        Player.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }

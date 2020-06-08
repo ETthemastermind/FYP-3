0f1301cffdf96dbe79c61200e27f6ee68589a52e
Ethan_Desktop <36960868+s4911672@users.noreply.github.com>
2020-06-08 15:05:07 +0100

    SRA BEGINSSSSS :D. Continuing work on the modality controller / main menu scenes

--- Assets/ModalityController2.cs
+++ Assets/ModalityController2.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+
+
 public class ModalityController2 : MonoBehaviour
 {
     public bool Gamepad_Chosen;
     public bool VRSR_Chosen;
     public bool VRLMSR_Chosen;
+
+    public bool TestingModeActivated;
+
+    public int SceneToLoad;
+    public GameObject IdentifierInputBox;
+    public GameObject NoModalitySelected;
+
+    public GameObject MainMenuCanvas;
+    public GameObject TestingCanvas;
+
+
+    //Testing Variables
+    public string UserIdentifier;
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(gameObject); //keeps the modality controller persistent between scenes
     }
 
     // Update is called once per frame
@@ -21,22 +37,110 @@ public class ModalityController2 : MonoBehaviour
 
     public void GamepadModality()
     {
-        Gamepad_Chosen = true;
-        Debug.Log("Level Loading");
-        SceneManager.LoadScene(1);
+        Gamepad_Chosen = true; //activates the boolean so that the controllers in the museum scenes reference the correct controller
+        VRSR_Chosen = false;
+        VRLMSR_Chosen = false;
+        NoModalitySelected.SetActive(false);
+
     }
 
     public void VRSRModality()
     {
+        Gamepad_Chosen = false; //activates the boolean so that the controllers in the museum scenes reference the correct controller
         VRSR_Chosen = true;
-        Debug.Log("Level Loading");
-        SceneManager.LoadScene(1);
+        VRLMSR_Chosen = false;
+        NoModalitySelected.SetActive(false);
     }
 
     public void VRSRLMModality()
     {
+        Gamepad_Chosen = false; //activates the boolean so that the controllers in the museum scenes reference the correct controller
+        VRSR_Chosen = false;
         VRLMSR_Chosen = true;
-        Debug.Log("Level Loading");
-        SceneManager.LoadScene(1);
+        NoModalitySelected.SetActive(false);
+
+    }
+
+    public void LoadLevel()
+    {
+        if (Gamepad_Chosen == false && VRSR_Chosen == false && VRLMSR_Chosen == false) //check to see if the user has chosen a modality
+        {
+            NoModalitySelected.SetActive(true); //show error message
+
+
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneToLoad);
+        }
+        /*
+        if (TestingModeActivated == false)
+        {
+            Debug.Log("Level Loading");
+            SceneManager.LoadScene(SceneToLoad);
+        }
+        */
+        
+    }
+
+    public void TestingModeButtonPress()
+    {
+        /*
+        if (IdentifierInputBox.GetComponent<TMP_InputField>().interactable == true)
+        {
+            IdentifierInputBox.GetComponent<TMP_InputField>().interactable = false;
+            TestingModeActivated = false;
+        }
+        else
+        {
+            IdentifierInputBox.GetComponent<TMP_InputField>().interactable = true;
+            TestingModeActivated = true;
+
+        }
+       */
+
+        if (TestingModeActivated == true)
+        {
+            TestingModeActivated = false;
+        }
+        else if (TestingModeActivated == false)
+        {
+            TestingModeActivated = true;
+        }
+    }
+
+    public void BeginPlay()
+    {
+        Debug.Log("Begin Play Pressed");
+
+        if (TestingModeActivated == false)
+        {
+            LoadLevel();
+        }
+        else if (TestingModeActivated == true)
+        {
+            Debug.Log("Turn Main Menu Canvas Off");
+            MainMenuCanvas.SetActive(false);
+            TestingCanvas.SetActive(true);
+
+        }
+    }
+
+    public void BeginTest()
+    {
+        
+    }
+
+    public void GetUserID()
+    {
+        Debug.Log("User ID Entered"); //prints to console when the value is changed
+        UserIdentifier = IdentifierInputBox.GetComponent<TMP_InputField>().text;
+    }
+
+    public void ReturnToMenu()
+    {
+        TestingCanvas.SetActive(false);
+        MainMenuCanvas.SetActive(true);
+
     }
 }

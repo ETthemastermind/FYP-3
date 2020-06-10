7abd022834b597be32342727eeaca59bb7a48c01
Ethan_Desktop <36960868+s4911672@users.noreply.github.com>
2020-06-10 13:16:28 +0100

    Fixed the player choosing for the museum scene, lobby built out more, halfway thur scripting tutorial. going to fix the audio playing over it self issue quickly

---
+++ Assets/ChooseCorrectPlayerMusScene.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseCorrectPlayerMusScene : MonoBehaviour
+{
+    public GameObject GamepadPlayer; //references for the players versions
+    public GameObject VRSRPlayer;
+    public GameObject VRLMSRPlayer;
+    // Start is called before the first frame update
+    public GameObject ModalityController;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        ModalityController = GameObject.FindGameObjectWithTag("ModalityController");
+        if (ModalityController.GetComponent<ModalityController2>().Gamepad_Chosen == true)
+        {
+           
+            
+            GamepadPlayer.SetActive(true);
+            VRSRPlayer.SetActive(false);
+            VRLMSRPlayer.SetActive(false);
+            
+
+            
+        }
+
+        else if (ModalityController.GetComponent<ModalityController2>().VRSR_Chosen == true)
+        {
+            GamepadPlayer.SetActive(false);
+            VRSRPlayer.SetActive(true);
+            VRLMSRPlayer.SetActive(false);
+
+
+        }
+
+        else if (ModalityController.GetComponent<ModalityController2>().VRLMSR_Chosen == true)
+        {
+            GamepadPlayer.SetActive(false);
+            VRSRPlayer.SetActive(false);
+            VRLMSRPlayer.SetActive(true);
+
+
+
+        }
+
+
+
+
+
+        
+    }
+}
--- Assets/TutorialController.cs
+++ Assets/TutorialController.cs
@@ -7,13 +7,16 @@ using TMPro;
 public class TutorialController : MonoBehaviour
 {
     public GameObject Player;
+    public GameObject PlayersNotebook;
     public AudioSource AS;
     private bool AudioPlayed;
+    private bool NotebookActive;
+    public GameObject NotebookController;
     //Phase 1 variables of the tutorial//
     public bool TriggerPhase1;
     public GameObject CuratorPortrait; //ref for the curator portrait
     public Material CuratorPortraitMat; //ref for the material on the portrait (woman with blurred musuem background
-    public GameObject InteractRing;
+    public GameObject PortraitInteractRing;
     public Color LerpedColour = Color.black; //ref for the lerped color for the emissive, black is no emissive
 
     public AudioClip HelloThere;
@@ -37,15 +40,20 @@ public class TutorialController : MonoBehaviour
     public bool TriggerPhase3;
     public GameObject Notebook;
     public Material NotebookMat;
+    public GameObject ArtifactInteractRing;
 
     //phase 4 variables
     public bool TriggerPhase4;
+    public AudioClip PickUp;
+    public GameObject PickUpController;
 
     //phase 5 variables
     public bool TriggerPhase5;
+    public AudioClip PutDown;
 
     //phase 6 variables
     public bool TriggerPhase6;
+    public AudioClip Guidebook;
 
     //phase 7 variables
     public bool TriggerPhase7;
@@ -64,9 +72,15 @@ public class TutorialController : MonoBehaviour
     void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
+        PlayersNotebook = GameObject.FindGameObjectWithTag("Notebook");
         AS = Player.GetComponent<AudioSource>();
+        PickUpController = GameObject.FindGameObjectWithTag("PickUpController");
+        NotebookController = GameObject.FindGameObjectWithTag("NotebookController");
+        PlayersNotebook.SetActive(false);
+        NotebookController.SetActive(false);
+
         CuratorPortraitMat = CuratorPortrait.GetComponent<Renderer>().materials[1]; //finds the correct mat on the portrait 
-        NotebookMat = Notebook.GetComponent<Renderer>().material;
+        NotebookMat = Notebook.GetComponent<Renderer>().materials[0];
         PortraitKeyword1.SetActive(false);
         PortraitKeyword2.SetActive(false);
         PortraitKeyword3.SetActive(false);
@@ -97,6 +111,18 @@ public class TutorialController : MonoBehaviour
         {
             Phase3();
         }
+        if (TriggerPhase4 == true)
+        {
+            Phase4();
+        }
+        if (TriggerPhase5 == true)
+        {
+            Phase5();
+        }
+        if (TriggerPhase6 == true)
+        {
+            Phase6();
+        }
 
     }
 
@@ -104,7 +130,7 @@ public class TutorialController : MonoBehaviour
     {
         Debug.Log("Phase 1 tutorial active");
 
-        if (AudioPlayed == false)
+        if (AudioPlayed == false && AS.isPlaying == false) //play instructions if clip is not played and no other clip is playing.
         {
             AS.PlayOneShot(HelloThere);
             AudioPlayed = true;
@@ -113,8 +139,8 @@ public class TutorialController : MonoBehaviour
         
 
         LerpedColour = Color.Lerp(Color.black, Color.yellow, Mathf.PingPong(Time.time, 1)); //lerp between black and yellow, gives a nice tutorial glowing effect
-        CuratorPortraitMat.SetColor("_EmissionColor", LerpedColour); //sets the emissive color of the portrait
-        if (InteractRing.GetComponent<AreaEntered>().PlayerInTrigger == true) //if the player enters the interact ring
+        CuratorPortraitMat.SetColor("_EmissionColor", LerpedColour); //sets the glow emissive color of the portrait
+        if (PortraitInteractRing.GetComponent<AreaEntered>().PlayerInTrigger == true) //if the player enters the interact ring, trigger phase 2
         {
             TriggerPhase2 = true;
             TriggerPhase1 = false;
@@ -127,13 +153,13 @@ public class TutorialController : MonoBehaviour
     public void Phase2() //introducing the "tell me about this"
     {
         Debug.Log("Phase 2 tutorial active");
-        if (AudioPlayed == false)
+        if (AudioPlayed == false && AS.isPlaying == false)
         {
             AS.PlayOneShot(TellMeAboutThis);
             AudioPlayed = true;
         }
         
-        string LastWordSaid = SpeechController.GetComponent<PortraitSpeechRec>().LastSaidWord;
+        string LastWordSaid = SpeechController.GetComponent<PortraitSpeechRec>().LastSaidWord; //gets the last word said from the speech controller
         TriggerPhase1 = false; //deactivates phase one
         CuratorPortraitMat.SetColor("_EmissionColor", Color.black);
        
@@ -143,7 +169,7 @@ public class TutorialController : MonoBehaviour
         //PortraitKeyword1.transform.FindChild[0].GetComponent<TextMeshProUGUI>().color = Color.Lerp(Color.white, Color.yellow, Mathf.PingPong(Time.time, 1));
         LastWordSaid = SpeechController.GetComponent<PortraitSpeechRec>().LastSaidWord;
         //Debug.Log(LastWordSaid + "said");
-        if (LastWordSaid == LastWordDefault) //checking to see if the user has done as requested
+        if (LastWordSaid == LastWordDefault) //checking to see if the user has done as requested, trigger phase 3 if yes
         {
             TriggerPhase3 = false;
 
@@ -161,24 +187,63 @@ public class TutorialController : MonoBehaviour
     public void Phase3() //Send them to pick-up artefact, make the artefact glow
     {
         Debug.Log("Phase 3 Tutorial Active");
+        NotebookMat.EnableKeyword("_EMISSION");
         LerpedColour = Color.Lerp(Color.black, Color.yellow, Mathf.PingPong(Time.time, 1));
         NotebookMat.SetColor("_EmissionColor", LerpedColour);
+
+        if (ArtifactInteractRing.GetComponent<AreaEntered>().PlayerInTrigger == true) //if user enters trigger area, trigger phase 4 
+        {
+            NotebookMat.SetColor("_EmissionColor", Color.black);
+            TriggerPhase3 = false;
+            TriggerPhase4 = true;
+
+        }
     }
 
     public void Phase4() // encourage to pick up
     {
+        if (AudioPlayed == false && AS.isPlaying == false)
+        {
+            AS.PlayOneShot(PickUp);
+            AudioPlayed = true;
+        }
 
+        if (PickUpController.GetComponent<Artefact_Hand_PickUp>().VR_HoldingObject == true) //if user picks up the artefact, trigger phase 5
+        {
+            TriggerPhase4 = false;
+            TriggerPhase5 = true;
+            AudioPlayed = false;
+
+        }
+        
 
     }
 
     public void Phase5() // encourage to put down
     {
+        if (AudioPlayed == false && AS.isPlaying == false)
+        {
+            AS.PlayOneShot(PutDown);
+            AudioPlayed = true;
+        }
 
+        if (PickUpController.GetComponent<Artefact_Hand_PickUp>().VR_HoldingObject == false) //if user puts down the artefact, trigger phase 5
+        {
+            TriggerPhase5 = false;
+            TriggerPhase6 = true;
+            AudioPlayed = false;
+
+        }
     }
 
     public void Phase6() //Show to request information 
     {
-
+        NotebookController.SetActive(true);
+        if (AudioPlayed == false && AS.isPlaying == false)
+        {
+            AS.PlayOneShot(Guidebook);
+            AudioPlayed = true;
+        }
     }
 
     public void Phase7() //send to the slider object

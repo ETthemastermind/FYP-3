623b1176126b0d8234735c2de1e4016f2b40a43e
Ethan_Desktop <36960868+s4911672@users.noreply.github.com>
2020-06-22 17:54:19 +0100

    pushing new telemetry system script

--- Assets/TelemetrySystemV2.cs
+++ Assets/TelemetrySystemV2.cs
@@ -8,14 +8,16 @@ public class TelemetrySystemV2 : MonoBehaviour
 {
 
     public string FilePath;
-    public string TestDataLog;
+    public string[] TestDataLog;
+    public bool TestSaveData;
+    public string[] DemographicInfo;
     // Start is called before the first frame update
     void Start()
     {
         DontDestroyOnLoad(gameObject);
-        FilePath = Application.persistentDataPath + "/Telemetry.txt";
+        FilePath = Application.persistentDataPath + "/Telemetry.csv";
         StreamWriter writer = new StreamWriter(FilePath, false);
-        writer.WriteLine("Spin my nipple nuts and send me to alaska");
+        //writer.WriteLine("Spin my nipple nuts and send me to alaska"); debug to test 
         writer.Close();
         
     }
@@ -23,12 +25,31 @@ public class TelemetrySystemV2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (TestSaveData == true)
+        {
+            AddEntry(TestDataLog);
+        }
     }
 
     public void AddEntry(string[] DataLog)
     {
+        string LogToEnter = ""; //clears the entry log
+        Debug.Log("Add Entry");
+        for (int d = 0; d < 2; d++) //for the first two entries in demographic info (identifier and version)
+        {
+            Debug.Log("Adding relevant demographic info");
+            LogToEnter += DemographicInfo[d] + ",";
+        }
+
+        for (int i = 0; i < DataLog.Length; i++)
+        {
+            LogToEnter += DataLog[i] + ",";
+        }
 
 
+        StreamWriter writer = new StreamWriter(FilePath, true);
+        writer.WriteLine(LogToEnter);
+        writer.Close();
+        TestSaveData = false;
     }
 }

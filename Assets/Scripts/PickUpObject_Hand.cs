9d28e53be8c120618bbf56eedf3da9ae8e0f28d6
Ethan_Desktop <36960868+s4911672@users.noreply.github.com>
2020-06-23 14:12:23 +0100

    picking up using SteamVR interactions etc works. checking the LM pickup now

--- Assets/Scripts/PickUpObject_Hand.cs
+++ Assets/Scripts/PickUpObject_Hand.cs
@@ -11,27 +11,51 @@ public class PickUpObject_Hand : MonoBehaviour
 
     public GameObject HaloGlow; // reference to the halo glow component attatched to the display
 
-    public GameObject ParentDisplay; //reference to the display pedestal itself
+    public GameObject Parent; //reference to the display pedestal itself
+
+
+
+
+
+
+
+    //new variables
+    public AudioClip PickUpNoise;
+    public AudioClip PutDownNoise;
+    public GameObject Player;
+    public AudioSource AS;
+
+
+    //ArtefactStartingVariables
+    public Vector3 Start_Location;
+    public Vector3 Start_Rotation;
+    
     // Start is called before the first frame update
     void Start()
     {
-        PickUpController = GameObject.FindGameObjectWithTag("PickUpController"); //finds the pick up controller attatched to the player
+        //PickUpController = GameObject.FindGameObjectWithTag("PickUpController"); //finds the pick up controller attatched to the player
+        Player = GameObject.FindGameObjectWithTag("Player");
+        AS = Player.GetComponent<AudioSource>();
         RB = gameObject.GetComponent<Rigidbody>(); //gets the rigid body from the artefact
 
 
-        ParentDisplay = gameObject.transform.parent.gameObject; //finds the parent display, so it can be returned to the right place
-        for (int i = 0; i < ParentDisplay.transform.childCount; i++) // find the halo component
+        Start_Location = gameObject.transform.position;
+        Start_Rotation = gameObject.transform.localEulerAngles;
+
+        /*
+        Parent = gameObject.transform.parent.gameObject; //finds the parent display, so it can be returned to the right place
+        for (int i = 0; i < Parent.transform.childCount; i++) // find the halo component
         {
-            if (ParentDisplay.transform.GetChild(i).gameObject.name == "Halo")
+            if (Parent.transform.GetChild(i).gameObject.name == "Halo")
             {
-                HaloGlow = ParentDisplay.transform.GetChild(i).gameObject;
+                HaloGlow = Parent.transform.GetChild(i).gameObject;
 
             }
 
         }
 
         HaloGlow.SetActive(false); //turn it off by default
-
+        */
     }
 
     // Update is called once per frame
@@ -40,6 +64,7 @@ public class PickUpObject_Hand : MonoBehaviour
         
     }
 
+    /*
     public void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Right Hand" || other.gameObject.tag == "VR_RightHand" || other.gameObject.tag == "VR_LeftHand") //if a hand is in the object's trigger area
@@ -65,6 +90,20 @@ public class PickUpObject_Hand : MonoBehaviour
             //PickUpController.GetComponent<Artefact_Hand_PickUp>().GetArtefact(PickUpController);
             HaloGlow.SetActive(false); //turns the halo off
         }
+    }
+
+    */
+
+    public void OnPickedUp()
+    {
+        AS.PlayOneShot(PickUpNoise);
+    }
+
+    public void OnPutDown()
+    {
+        gameObject.transform.position = Start_Location;
+        gameObject.transform.localEulerAngles = Start_Rotation;
+        AS.PlayOneShot(PutDownNoise);
     }
 
    

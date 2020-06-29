5052937b1d82f4bea1870865dac6672f0ad5e7e6
Ethan_Desktop <36960868+s4911672@users.noreply.github.com>
2020-06-29 12:27:06 +0100

    push before making some changes to the Leap Motion add on scripts

---
+++ Assets/Scripts/SpawnGuideBookLM.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGuideBookLM : MonoBehaviour
+{
+    public GameObject GuideBook;
+    public GameObject Palm;
+    public bool ActiveGesture = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GuideBook.transform.parent = Palm.transform;
+        GuideBook.transform.localPosition = new Vector3(0.2f, -0.05f, 0f);
+        GuideBook.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Active()
+    {
+        if (ActiveGesture == false)
+        {
+            GuideBook.GetComponent<NotebookTelemetrySystem>().PushData("Guidebook Opened");
+            Debug.Log("Active Gesture: Spawn Guidebook");
+            GuideBook.SetActive(true);
+            ActiveGesture = true;
+        }
+
+
+
+
+    }
+
+    public void Inactive()
+    {
+
+        if (ActiveGesture == true)
+        {
+            GuideBook.GetComponent<NotebookTelemetrySystem>().PushData("Guidebook Closed");
+            ActiveGesture = false;
+            Debug.Log("Inactive Gesture: Spawn Guidebook");
+            GuideBook.SetActive(false);
+        }
+
+
+
+
+
+    }
+}

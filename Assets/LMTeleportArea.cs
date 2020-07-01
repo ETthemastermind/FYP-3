f63ebe5e8c8db5c22200fed6c267e661305379e4
Ethan_Desktop <36960868+s4911672@users.noreply.github.com>
2020-07-01 23:42:21 +0100

    added floor colliders so teleport around diorama scenes in vr+ can happen

---
+++ Assets/LMTeleportArea.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LMTeleportArea : MonoBehaviour
+{
+    public GameObject ModalityController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ModalityController = GameObject.FindGameObjectWithTag("ModalityController");
+        if (ModalityController.GetComponent<ModalityController2>().VRLMSR_Chosen != true)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
